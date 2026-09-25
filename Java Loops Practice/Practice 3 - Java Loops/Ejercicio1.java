c33c7aa1202309f1e34d1c4a09d72dacbd430df3
EmilianoLopezCruz <a2253332354@alumnos.uat.edu.mx>
2026-09-24 23:12:05 -0600

    Delete Java Loops Practice directory

--- Java Loops Practice/Practice 3 - Java Loops/Ejercicio1.java
+++
@@ -1,9 +0,0 @@
-public class Ejercicio1 {
-    public static void main(String[] args) {
-        // Ejercicio 1: Rastreo del bucle for de 2 a 12
-        for (int count = 2; count <= 12; count++) {
-            System.out.print(count + " ");
-        }
-        System.out.println();
-    }
-}
